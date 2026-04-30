5351ac9674b724120616b29801a89ec210d7393d
rlusnikov <rlusnikov@moysklad.ru>
2026-04-30 09:48:20 +0300

    AS-4487 AS-4591 добавить очистку старых значений в таблице JWT

--- src/php/lib/jwt-repo.php
+++ src/php/lib/jwt-repo.php
@@ -4,8 +4,16 @@ require_once __DIR__ . '/repo.php';
 
 class JwtSqliteRepository extends SqliteRepository
 {
+    // Время хранения старых записей в таблице: 24 часа
+    private const JTI_TTL_SECONDS = 86400;
+
+    // Вероятность запуска очистки (на production лучше установить <= 5)
+    private const DELETE_EXPIRED_CHANCE_PERCENT = 50;
+
     public function register(string $jti): bool
     {
+        $this->deleteExpiredMaybe();
+
         $stmt = $this->connection()->prepare(
             'INSERT OR IGNORE INTO jwt (
                 jti,
@@ -22,6 +30,28 @@ class JwtSqliteRepository extends SqliteRepository
         ]);
 
         return $stmt->rowCount() === 1;
+    }
+
+    public function deleteExpired(): void
+    {
+        $stmt = $this->connection()->prepare(
+            'DELETE FROM jwt
+            WHERE create_time < :expired_before'
+        );
+
+        $stmt->execute([
+            ':expired_before' => gmdate('c', time() - self::JTI_TTL_SECONDS),
+        ]);
+    }
+
+    private function deleteExpiredMaybe(): void
+    {
+        // Для оптимизации чтобы не выполнять очистку при каждой регистрации, делаем ее только иногда
+        if (mt_rand(1, 100) > self::DELETE_EXPIRED_CHANCE_PERCENT) {
+            return;
+        }
+
+        $this->deleteExpired();
     }
 
     protected function initializeSchema(PDO $pdo): void
